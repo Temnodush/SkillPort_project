73bdcf300df2e4cecb3462646700e73841e544e7
Temnodush <temnodush@vk.com>
2026-08-08 23:24:14 +0500

    ДЗ 30.1

---
+++ education/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name="название")
+    preview = models.ImageField(upload_to="courses/previews/", blank=True, null=True, verbose_name="превью")
+    description = models.TextField(blank=True, null=True, verbose_name="описание")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="курс",
+    )
+    title = models.CharField(max_length=255, verbose_name="название")
+    description = models.TextField(blank=True, null=True, verbose_name="описание")
+    preview = models.ImageField(upload_to="lessons/previews/", blank=True, null=True, verbose_name="превью")
+    video_url = models.URLField(blank=True, null=True, verbose_name="ссылка на видео")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "урок"
+        verbose_name_plural = "уроки"
